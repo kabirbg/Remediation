5a3b402b9d2fcf43ec97ca373f1418ab95190e41
Kabir Gupta <kabirg108@gmail.com>
2024-05-09 22:51:45 -0400

    init commit

---
+++ src/main/java/com/kabirbg/Remediation/RemediationApplication.java
@@ -0,0 +1,13 @@
+package com.kabirbg.Remediation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemediationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemediationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kabirbg/Remediation/RemediationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kabirbg.Remediation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemediationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
