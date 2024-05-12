6b7d8b82c9bdb3f454b1ca899ec6ac9e4b31eebe
Kabir Gupta <kabirg108@gmail.com>
2024-05-11 22:13:45 -0400

    success at long last!!! just need to add the right audio now

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
-  /* CONSTANTS DECLARATIONS */
 
+  /* CONSTANTS DECLARATIONS */
   const container = document.querySelector('.grid-container');
+  const images = document.querySelectorAll('.grid-image');
   const people = [ 'T', 'W', 'E', 'L', 'V', 'E', 'P', 'E', 'O', 'P', 'L', 'E' ];
   const instructions = [ "each", "to", "repeat", "for", "one", "minute",
     "constantly", "the", "following", "noise", "making", "onomatopoeia" ];
@@ -22,46 +22,95 @@ document.addEventListener('DOMContentLoaded', function() {
   ];
 
   /* MAIN BODY */
-  insertImages("people", people, 0); //100
-  setTimeout(() => shiftImages("people"), 0); //5000
-  setTimeout(() => insertImages("instructions", instructions, 75), 0); //7000
-  setTimeout(() => shiftImages("instructions"), 0); //14000
-  setTimeout(() => insertImages("nmot", nmot, 35), 0); //16000
+  var timerNeverSet = true;
 
-
+  insertImages("people", people, 70);
+  setTimeout(() => shiftImages("people"), 5000);
+  setTimeout(() => insertImages("instructions", instructions, 60), 8000);
+  setTimeout(() => shiftImages("instructions"), 13000);
+  setTimeout(initializeNMOT, 16000);
 
   /* FUNCTIONS DECLARATIONS */
 
-  // Add 12 images of a pattern
-  function insertImages(name, arr, height) {
-    for (let i = 1; i <= 12; i++) {
-      const img = document.createElement("img");
-      img.src = `resources/${name}/${i}.png`;
-      img.alt = arr[i - 1];
-      img.height = height;
-      img.classList.add(`image${i}`); // Add CSS class for styling
-      container.appendChild(img);
-    }
+  function initializeNMOT() { // Add audio elements and set up for NMOT interface
+    insertImages("nmot", nmot, 35);
+
+    document.querySelectorAll('.playbutton').forEach((button, index) => {
+      button.style.opacity = 1;
+      button.addEventListener('click', () => {
+        if (timerNeverSet) countdown();
+        const audioLeft = document.getElementById(`audio${index+1}left`);
+        const audioRight = document.getElementById(`audio${index+1}right`);
+        if (audioLeft.paused || audioRight.paused) {
+          button.src = "resources/pause.png";
+          button.alt = "Pause";
+          audioLeft.play();
+          audioRight.play();
+        } else { // button.alt == "Pause"
+          button.src = "resources/play.png";
+          button.alt = "Play";
+          audioLeft.pause();
+          audioRight.pause();
+        }
+      });
+    })
+
+    document.querySelectorAll('.slider').forEach((mixer, index) => {
+      mixer.style.opacity = 1;
+      mixer.addEventListener('input', function() {
+        const volumeValue = parseInt(mixer.value);
+        const audioLeft = document.getElementById(`audio${index+1}left`);
+        const audioRight = document.getElementById(`audio${index+1}right`);
+        audioLeft.volume = 1 - volumeValue / 100;
+        console.log(audioLeft.volume);
+        audioRight.volume = volumeValue / 100;
+        console.log(audioRight.volume);
+      });
+    })
+  }
+
+  function insertImages(name, arr, height) { // Add 12 images of a pattern
+    images.forEach((image, index) => {
+      image.src = `resources/${name}/${index + 1}.png`;
+      image.alt = arr[index];
+      image.height = height;
+    })
     fadeImages();
   }
 
-  // Replace the grid of images images with their copies at the top
-  function shiftImages(id){
+  function shiftImages(id) { // Replace the grid of images images with their copies at the top
       fadeImages(fadeOut=true); // Trigger fade-out animation
-      container.innerHTML = ''; // Clear existing images from the container
-      document.querySelectorAll(`#${id}`).forEach(element => {
+      // Show images outside the grid
+      setTimeout(() => { 
+        document.querySelectorAll(`#${id}`).forEach(element => {
         element.style.opacity = '1'; // Set opacity of the actual images to 1
         element.style.transition = 'opacity 1s ease-in-out' // Fade in
+        }, 3000);
       });
   }
 
-  // Fade images in/out
-  function fadeImages(fadeOut=false) {
-    const images = document.querySelectorAll('.grid-container img');
+  function fadeImages(fadeOut=false) { // Fade images in/out
+    //const images = document.querySelectorAll('.grid-container img');
     images.forEach((image, index) => {
       setTimeout(() => {
         image.style.opacity = fadeOut ? 0 : 1; // Fade out each image
-      }, index * 250); // Delay each image by 250 ms for staggered fade-in
+      }, index * 250); // Delay each image by 250 ms for staggered fade
     });
+  }
+
+  function countdown() { // Start the one-minute countdown
+    timerNeverSet = false;
+    document.getElementById('timer').style.opacity = 1;
+    var seconds = 60, timer = setTimeout(update, 1000);
+    function update()
+    {
+      if (seconds > 0) {
+      document.getElementById("timer").innerText = "You have " + --seconds + " seconds remaining.";
+         timer = setTimeout(update, 1000);
+      } else {
+          alert('Your one minute of noise-making onomatopoeia is over. Close the page or reload to play again.');
+          location.reload();
+        }
+    }
   }
 });
